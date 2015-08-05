bd0280bfbe8c2703422de1afd6067c2dac170045
Suyesh Bhandari <suyeshb@gmail.com>
2015-08-05 01:22:50 -0400

    added first model

---
+++ db/migrate/20150805044752_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :rating
+      t.decimal :total_gross
+
+      t.timestamps null: false
+    end
+    add_index :movies, :title
+  end
+end
