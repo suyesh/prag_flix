a8586a85b6e751996f59afbe23b778da0b23f480
Suyesh Bhandari <suyeshb@gmail.com>
2015-08-06 18:07:53 -0400

    Some edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805044752) do
+ActiveRecord::Schema.define(version: 20150806213304) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20150805044752) do
     t.decimal  "total_gross"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.date     "released_on"
   end
 
   add_index "movies", ["title"], name: "index_movies_on_title"
