7efb6d5e55eb3b59ce8ef8a5e8982de4ab82dd4b
Suyesh Bhandari <suyeshb@gmail.com>
2015-08-16 21:02:42 -0400

    Added new controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,8 +13,22 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).permit(:title,:description, :rating,:released_on,:total_gross)
     @movie.update(movie_params)
     redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
