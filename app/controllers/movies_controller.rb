8e897e285c3622f535b0f57366e1dbc617eba933
Suyesh Bhandari <suyeshb@gmail.com>
2015-08-17 19:17:03 -0400

    Styling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,11 +6,11 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
-
+  
   def edit
     @movie = Movie.find(params[:id])
   end
-
+  
   def update
     @movie = Movie.find(params[:id])
     @movie.update(movie_params)
@@ -20,21 +20,23 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
-
+  
   def create
-    @movie = Movie.create(movie_params)
+    @movie = Movie.new(movie_params)
+    @movie.save
     redirect_to @movie
   end
-
+  
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
     redirect_to movies_url
   end
-
-  private
+  
+private
 
   def movie_params
     params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
   end
 end
+
