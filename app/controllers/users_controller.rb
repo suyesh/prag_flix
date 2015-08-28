cc0bc79f3013e80a9183a0b6ceb33f8862f0ca1d
Suyesh Bhandari <suyeshb@gmail.com>
2015-08-27 22:49:38 -0400

    Authentication 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
 	before_action :require_signin, except: [:new, :create]
+	before_action :require_correct_user, only: [:edit, :update, :destroy]
 
 	def index
 		@users = User.all
@@ -25,11 +26,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(set_user_params)
 			redirect_to user_path(@user), notice: "Account successfully Updated!"
 		else
@@ -38,7 +37,6 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		@user = User.find(params[:id])
 		@user.destroy
 		session[:user_id] = nil
 		redirect_to root_path, alert: "You have successfully deleted your account"
@@ -48,6 +46,11 @@ class UsersController < ApplicationController
 
 	def set_user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation, :username)
+	end
+
+	def require_correct_user
+		@user = User.find(params[:id])
+		redirect_to root_url unless current_user?(@user)
 	end
 
 end
